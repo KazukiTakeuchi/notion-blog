152b3c20311d94b54185118044bb9ab51323fb26
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-04-01 15:29:54 +0900

    remove: 使用していないSNSアイコンを削除

--- site.config.ts
+++ site.config.ts
@@ -1,6 +1,4 @@
 export const siteConfig = {
   title: "Kazuki Takeuchi Portfolio",
-  twitterUrl: "https://twitter.com/username",
-  instagramUrl: "https://instagram.com/username",
   githubUrl: "https://github.com/KazukiTakeuchi",
 };
