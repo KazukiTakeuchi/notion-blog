bd635bb156b93d4bf51573d910b68cae04a579f7
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-04-09 23:48:56 +0900

    feat: notionのarticleからデータを取得する

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,25 @@
-import Card from "@/components/Card";
-import { siteConfig } from "@/site.config";
-import { fetchPages } from "@/utils/notion";
-import { sampleCards } from "@/utils/sample";
 import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
+import Card from "../components/Card";
 import Layout from "../components/Layout";
+import { siteConfig } from "../site.config";
+import { fetchPages } from "../utils/notion";
+import { sampleCards } from "../utils/sample";
 
 export const getStaticProps: GetStaticProps = async () => {
   const { results } = await fetchPages();
-  return  {
+  return {
     props: {
       pages: results ? results : [],
     },
     revalidate: 10,
-  }
-}
+  };
+};
 
-const Home: NextPage = () => {
+const Home: NextPage = ({ pages }) => {
+  console.log(pages);
   return (
     <Layout>
       <div className="pt-12">
