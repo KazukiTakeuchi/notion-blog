8d6a5b50bc98e2e2abd717b98b4c2eabb4c1a2d7
“KazukiTakeuchi” <takeuchi1227813813@gmail.com>
2023-04-09 23:44:18 +0900

    chore: notionhq/clientでNotionCMSのDBと接続する

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,22 @@
 import Card from "@/components/Card";
 import { siteConfig } from "@/site.config";
+import { fetchPages } from "@/utils/notion";
 import { sampleCards } from "@/utils/sample";
-import type { NextPage } from "next";
+import type { GetStaticProps, NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
 import Link from "next/link";
 import Layout from "../components/Layout";
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { results } = await fetchPages();
+  return  {
+    props: {
+      pages: results ? results : [],
+    },
+    revalidate: 10,
+  }
+}
 
 const Home: NextPage = () => {
   return (
